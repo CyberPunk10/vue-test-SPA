858b932cf94aea4eb41485c468647c66790633f0
CyberPunk10 <konstantin21_arx@gmail.com>
2020-08-15 19:59:23 +0300

    Development Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+nav
+  .nav-wrapper
+    <router-link to="/" class="brand-logo left">Tasks</router-link>
+    ul(id="nav-mobile" class="right hide-on-med-and-down")
+      <router-link tag="li" to="/" exact active-class="active"></router-link>
+        a(href="#") Create
+      <router-link tag="li" to="/" active-class="active"></router-link>
+        a(href="#") List
+
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
