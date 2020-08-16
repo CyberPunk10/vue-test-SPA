3a9d82b0bc081c91e8f798ef0b1fccd801c0b479
CyberPunk10 <konstantin21_arx@gmail.com>
2020-08-16 19:43:26 +0300

    Development views List & Task

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ export default createStore({
   },
 
   getters: {
-    tasks: state => state.tasks
+    tasks: state => state.tasks,
+    taskById: s => id => s.tasks.find(t => t.id === id)
   },
 
   mutations: {
@@ -14,13 +15,31 @@ export default createStore({
       state.tasks.push(task)
 
       localStorage.setItem('tasks', JSON.stringify(state.tasks))
+    },
+    updateTask (s, { id, description, date }) {
+      const tasks = s.tasks.concat()
+
+      const idx = tasks.findIndex(t => t.id === +id)
+      const task = tasks[idx]
+
+      const status = new Date(date) > new Date() ? 'active' : 'outdated'
+
+      tasks[idx] = { ...task, date, description, status }
+
+      s.tasks = tasks
+
+      localStorage.setItem('tasks', JSON.stringify(s.tasks))
     }
   },
 
   actions: {
     createTask ({ commit }, task) {
       commit('createTask', task)
+    },
+    updateTask ({ commit }, task) {
+      commit('updateTask', task)
     }
+
   },
 
   modules: {
